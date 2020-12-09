25371ab19b9e02e5f5420f36dfacb1f9d85af8a6
Alessio Gambi <alessio.gambi@uni-passau.de>
2020-12-09 14:52:54 +0100

    Commented one test generator code, added minimal description

--- sample_test_generators/one_test_generator.py
+++ sample_test_generators/one_test_generator.py
@@ -1,7 +1,12 @@
-from random import randint
+import numpy as np
+import math
 
 
 class OneTestGenerator():
+    """
+        Generates a single test to show how to control the shape of the road by controlling the positio of the
+        road points. We assume a map of 200x200
+    """
 
     def __init__(self, time_budget=None, executor=None, map_size=None):
         self.time_budget = time_budget
@@ -10,22 +15,45 @@ class OneTestGenerator():
 
     def start(self):
         print("Starting test generation")
-        # Generate a single test.
 
         test = []
 
-        test.append( (0, 0) )
-        # Curves cannot be too sharp?
-        test.append( (0, 80) )
-        test.append( (0, 100) )
-        test.append( (100, 100) )
-        test.append( (100, 0) )
+        # Create a vertical segment starting close to the left edge of the map
+        x = 10.0
+        y = 10.0
+        length = 100.0
+        interpolation_points = int(length / 10.0)
+        for y in np.linspace(y, y + length, num=interpolation_points):
+            test.append((x, y))
 
-        print("Generated test: ", test)
+        # Create the 90-deg right turn
+        radius = 20.0
+
+        center_x = x + radius
+        center_y = y
+
+        interpolation_points = 5
+        angles_in_deg = np.linspace(-60.0, 0.0, num=interpolation_points)
+
+        for angle_in_rads in [ math.radians(a) for a in angles_in_deg]:
+            x = math.sin(angle_in_rads) * radius + center_x
+            y = math.cos(angle_in_rads) * radius + center_y
+            test.append((x, y))
+
+        # Create an horizontal segment, make sure the points line up with previous segment
+        x += radius / 2.0
+        length = 30.0
+        interpolation_points = int(length / 10.0)
+        for x in np.linspace(x, x + length, num=interpolation_points):
+            test.append((x, y))
+
+        # Now we add a final road point "below" the last one just to illustrate how the interpolation works
+        y -= 50.0
+        test.append((x, y, -28.0, 8.0))
+
+        # Send the test for execution
         test_outcome, description, execution_data = self.executor.execute_test(test)
 
+        # Print test outcome
         print(test_outcome, description)
-        #print(execution_data)
-        # Check that data is timestamped
-        pass
 
