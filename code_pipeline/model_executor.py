4a71da36ef05040c139956f12e33909562971fa8
ziegltob <tobias.ziegler1996@gmx.de>
2021-06-29 16:52:03 +0200

    [model-executor] fix image processing

--- code_pipeline/model_executor.py
+++ code_pipeline/model_executor.py
@@ -2,10 +2,8 @@ from code_pipeline.executors import AbstractTestExecutor
 
 import time
 import traceback
-from typing import Tuple
 
 from self_driving.beamng_brewer import BeamNGBrewer
-# maps is a global variable in the module, which is initialized to Maps()
 from self_driving.beamng_tig_maps import maps, LevelsFolder
 from self_driving.beamng_waypoint import BeamNGWaypoint
 from self_driving.simulation_data import SimulationDataRecord, SimulationData
@@ -13,7 +11,8 @@ from self_driving.simulation_data_collector import SimulationDataCollector
 from self_driving.utils import get_node_coords, points_distance
 from self_driving.vehicle_state_reader import VehicleStateReader
 from beamngpy.sensors import Camera
-from PIL import Image
+import tensorflow as tf
+from tensorflow import keras
 
 from shapely.geometry import Point
 
@@ -21,6 +20,15 @@ import logging as log
 import os.path
 import cv2
 import numpy
+
+
+def preprocess_image(image, resize=None):
+    image_array = numpy.asarray(image)
+    # removes sky and front of car
+    image_array = image_array[80:-1, :, :]
+    image_array = cv2.resize(image_array, resize, interpolation=cv2.INTER_AREA)
+    image_array = cv2.cvtColor(image_array, cv2.COLOR_RGB2YUV)
+    return image_array
 
 
 class ModelExecutor(AbstractTestExecutor):
@@ -50,6 +58,15 @@ class ModelExecutor(AbstractTestExecutor):
         self.min_delta_position = 1.0
         self.road_visualizer = road_visualizer
         self.driver_camera_name = 'driver_view_camera'
+
+        self.model: tf.keras.Model = tf.keras.models.load_model(model_path)
+        # self.model: tf.keras.Model = tf.keras.models.model_from_json(model_path)
+        self.model.compile()
+        # weights_file = model_path
+        # self.model.load_weights(weights_file)
+
+        # Check model architecture
+        self.model.summary()
 
     def _execute(self, the_test):
         # Ensure we do not execute anything longer than the time budget
@@ -116,9 +133,6 @@ class ModelExecutor(AbstractTestExecutor):
             self.brewer = BeamNGBrewer(beamng_home=self.beamng_home, beamng_user=self.beamng_user)
             self.vehicle = self.brewer.setup_vehicle()
 
-        # lidar = Lidar()
-        # self.vehicle.attach_sensor('lidar', lidar)
-
         # For the execution we need the interpolated points
         nodes = the_test.interpolated_points
 
@@ -132,12 +146,11 @@ class ModelExecutor(AbstractTestExecutor):
             beamng_levels = LevelsFolder(os.path.join(self.beamng_user, 'levels'))
             maps.beamng_levels = beamng_levels
             maps.beamng_map = maps.beamng_levels.get_map('tig')
-            # maps.print_paths()
 
         maps.install_map_if_needed()
         maps.beamng_map.generated().write_items(brewer.decal_road.to_json() + '\n' + waypoint_goal.to_json())
 
-        camera = (self.driver_camera_name, Camera((-0.3, 2.1, 1), (0, 1, 0), 90, (600, 600)))
+        camera = (self.driver_camera_name, Camera((-0.3, 2.1, 1), (0, 1, 0), 120, (66, 200)))
         additional_sensors = [camera]
         vehicle_state_reader = VehicleStateReader(self.vehicle, beamng, additional_sensors=additional_sensors)
         brewer.vehicle_start_pose = brewer.road_points.vehicle_start_pose()
@@ -154,33 +167,37 @@ class ModelExecutor(AbstractTestExecutor):
 
         sim_data_collector.get_simulation_data().start()
         try:
-            # start = timeit.default_timer()
             brewer.bring_up()
-            # iterations_count = int(self.test_time_budget/250)
-            # idx = 0
-
-            brewer.vehicle.ai_set_aggression(0.7)
-            # TODO This does not seem to take any effect...
-            brewer.vehicle.ai_set_speed(self.maxspeed, mode='limit')
-            brewer.vehicle.ai_drive_in_lane(False)
-            brewer.vehicle.ai_set_waypoint(waypoint_goal.name)
 
             while True:
-                # idx += 1
-                # assert idx < iterations_count, "Timeout Simulation " + str(sim_data_collector.name)
-
                 img = self.get_driver_camera_image()
+                img_array = preprocess_image(img, (200, 66))
+                img_array = tf.cast(img_array, tf.float32)
+                img_array = numpy.array([img_array])
+                predicted_values = self.predict(img_array, ['steering_angle'], batch=1)
+                steering_angle = float(predicted_values)
+                self.vehicle.control(steering=steering_angle)
 
-                sim_data_collector.collect_current_data(oob_bb=True)
+                # show driver view with predicted steering angle
+                cv2.putText(img, f"{steering_angle:.9f}", (2, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1,
+                            cv2.LINE_AA)
+                cv2.imshow('Predicted Steering Angle', img)
+                cv2.waitKey(10)
+
+                sim_data_collector.collect_current_data(oob_bb=False)
                 last_state: SimulationDataRecord = sim_data_collector.states[-1]
                 # Target point reached
+                if last_state.vel_kmh > self.maxspeed:
+                    self.vehicle.control(throttle=0)
+                else:
+                    self.vehicle.control(throttle=0.3)
                 if points_distance(last_state.pos, waypoint_goal.position) < 8.0:
                     break
 
                 assert self._is_the_car_moving(last_state), "Car is not moving fast enough " + str(
                     sim_data_collector.name)
 
-                assert not last_state.is_oob, "Car drove out of the lane " + str(sim_data_collector.name)
+                # assert not last_state.is_oob, "Car drove out of the lane " + str(sim_data_collector.name)
 
                 beamng.step(steps)
 
@@ -210,14 +227,15 @@ class ModelExecutor(AbstractTestExecutor):
         return sim_data_collector.simulation_data
 
     def get_driver_camera_image(self):
-        # self.camera.pos = self.pose.pos
-        # self.camera.direction = self.pose.rot
         sensors = self.brewer.beamng.poll_sensors(self.vehicle)
         cam = sensors[self.driver_camera_name]
         img = cam['colour'].convert('RGB')
-        # TODO: maybe convert to opencv format and feed model with image
-        img.show()
-        return img
+        img_cv = cv2.cvtColor(numpy.asarray(img), cv2.COLOR_RGB2BGR)
+        return img_cv
+
+    def predict(self, image, properties=None, batch=None):
+        predicted_values = self.model.predict(image, batch_size=batch)
+        return predicted_values
 
     def end_iteration(self):
         try:
