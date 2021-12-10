e433bfabf3de169f161757dea39d7900731879e8
Alessio Gambi <alessiogambi@gmail.com>
2021-12-10 11:56:36 +0100

    Moved dave2 model in a new folder. Updated test cases

--- tests/commandline_parsing_tests.py
+++ tests/commandline_parsing_tests.py
@@ -3,6 +3,8 @@ from click.testing import CliRunner
 
 from competition import generate
 
+# FOR SOME REASONS THOSE TESTS WORK ONLY IF WE START ONE AFTER THE OTHER MANUALLY
+# I SUSPECT THE TEST DO NOT WAY FOR THE runner TO FINISH
 class CommandLineCombinationTest(unittest.TestCase):
 
     def test_fail_when_model_is_missing(self):
@@ -15,14 +17,19 @@ class CommandLineCombinationTest(unittest.TestCase):
     def test_do_not_fail_when_model_is_missing_but_executor_is_different(self):
         runner = CliRunner()
         result = runner.invoke(generate, ['--executor', 'mock', '--time-budget', '10', '--map-size', '200', '--module-name', 'foo', '--class-name', 'Foo'])
+        print("1" + result.output)
         assert "Started test generation" in result.output
+
 
     def test_do_fail_when_model_is_present_but_executor_is_different(self):
         runner = CliRunner()
-        result = runner.invoke(generate, ['--executor', 'mock', '--dave2-model', '../code_pipeline/self-driving-car-010-2020.h5', '--time-budget', '10', '--map-size', '200', '--module-name', 'foo', '--class-name', 'Foo'])
+        result = runner.invoke(generate, ['--executor', 'mock', '--dave2-model', '../dave2/self-driving-car-010-2020.h5', '--time-budget', '10', '--map-size', '200', '--module-name', 'foo', '--class-name', 'Foo'])
+        print("2" + result.output)
         assert "Started test generation" in result.output
+
 
     def test_do_not_fail_when_model_is_present(self):
         runner = CliRunner()
-        result = runner.invoke(generate, ['--executor', 'dave2', '--dave2-model', '../code_pipeline/self-driving-car-010-2020.h5', '--time-budget', '10', '--map-size', '200', '--module-name', 'foo', '--class-name', 'Foo'])
+        result = runner.invoke(generate, ['--executor', 'dave2', '--dave2-model', '../dave2/self-driving-car-010-2020.h5', '--time-budget', '10', '--map-size', '200', '--module-name', 'foo', '--class-name', 'Foo'])
+        print("3" + result.output)
         assert "Started test generation" in result.output
